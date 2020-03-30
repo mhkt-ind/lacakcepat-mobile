27f60b6b04227dc24a326eadd1fa4e10bbd913e1
Leguna <ahmadtuflihunxd@gmail.com>
2020-03-30 23:12:34 +0700

    fix bug backstack

--- app/src/main/java/id/lacakcepat/covidnineteen/ui/activity/MainActivity.kt
+++ app/src/main/java/id/lacakcepat/covidnineteen/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package id.lacakcepat.covidnineteen.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import id.lacakcepat.covidnineteen.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private var doubleBackToExitPressedOnce = false
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            finishAffinity()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
     }
 }
