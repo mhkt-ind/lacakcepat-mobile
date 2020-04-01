83f9b6f856c7f227d4b7973ff1039c2e5f22659b
Indra Mahkota <indramahkota1@gmail.com>
2020-04-02 01:33:44 +0700

    init pop up

--- app/src/main/java/id/lacakcepat/covidnineteen/ui/activity/MainActivity.kt
+++ app/src/main/java/id/lacakcepat/covidnineteen/ui/activity/MainActivity.kt
@@ -1,20 +1,56 @@
 package id.lacakcepat.covidnineteen.ui.activity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.ColorUtils
+import androidx.core.view.forEach
 import id.lacakcepat.covidnineteen.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var isPopUp = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(0, 0)
         setContentView(R.layout.activity_main)
+
+        nav_view.menu.forEach { it.isEnabled = false }
+
+        val alpha = 100
+        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
+        colorAnimation.duration = 500
+        colorAnimation.addUpdateListener { animator ->
+            popup_window_background.setBackgroundColor(animator.animatedValue as Int)
+        }
+        colorAnimation.start()
+
+        popup_window_view_with_border.alpha = 0f
+        popup_window_view_with_border.animate().alpha(1f).setDuration(500).setInterpolator(
+            DecelerateInterpolator()
+        ).start()
+    }
+
+    override fun onBackPressed() {
+        if(isPopUp) {
+            removePopUpHandler()
+        } else {
+            doubleBackHandler()
+        }
     }
 
     private var doubleBackToExitPressedOnce = false
-    override fun onBackPressed() {
+    private fun doubleBackHandler() {
         if (doubleBackToExitPressedOnce) {
             finishAffinity()
             return
@@ -22,7 +58,31 @@ class MainActivity : AppCompatActivity() {
 
         this.doubleBackToExitPressedOnce = true
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+    }
 
-        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    private fun removePopUpHandler() {
+        val alpha = 100
+        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), alphaColor, Color.TRANSPARENT)
+        colorAnimation.duration = 500
+        colorAnimation.addUpdateListener { animator ->
+            popup_window_background.setBackgroundColor(
+                animator.animatedValue as Int
+            )
+        }
+
+        popup_window_view_with_border.animate().alpha(0f).setDuration(500).setInterpolator(
+            DecelerateInterpolator()
+        ).start()
+
+        colorAnimation.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                isPopUp = false
+                nav_view.menu.forEach { it.isEnabled = true }
+                overridePendingTransition(0, 0)
+            }
+        })
+        colorAnimation.start()
     }
 }
